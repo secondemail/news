45c31eb8a43e5a8e8ce76c984305917d89133e28
secondemail <166629198+secondemail@users.noreply.github.com>
2024-04-26 13:24:19 +0300

    Update HomePage.js

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Container, Spinner } from 'react-bootstrap'
 import HomePosts from '../../Components/Home/HomePosts'
 import HomeLatestNews from './../../Components/Home/HomeLatestNews';
 import HomeEconomic from '../../Components/Home/HomeEconomic';
@@ -9,21 +9,37 @@ import HomePrivate from '../../Components/Home/HomePrivate';
 import HomeGallerey from '../../Components/Home/HomeGallerey';
 import { Bounce, Fade, Slide } from 'react-awesome-reveal';
 
-const HomePage=()=> {
+const HomePage = () => {
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 1200);
+    }, [])
+    
+    
     return (
         <Container>
-            <Fade>
-                <HomeLatestNews/>
-                <Bounce>
-                    <HomeEconomic/>
-                </Bounce>
-                <HomeCollection />
-                <Slide>
-                    <InfoGraphic/>
-                </Slide>
-                    <HomePrivate/>
-                <HomeGallerey/>
-            </Fade>
+            {!loading ?
+                (
+                    <Fade>
+                        <HomeLatestNews/>
+                        <Bounce>
+                            <HomeEconomic/>
+                        </Bounce>
+                        <HomeCollection />
+                        <Slide>
+                            <InfoGraphic/>
+                        </Slide>
+                            <HomePrivate/>
+                        <HomeGallerey/>
+                    </Fade>
+                ) : (
+                    <div className='text-center'>
+                        <Spinner style={{padding:"70px",fontSize:"20px"}} animation="border" variant="primary" size='large' />
+                    </div>
+                )
+            }
         </Container>
     )
 }
